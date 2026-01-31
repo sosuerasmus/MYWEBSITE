5a0ebf79865c804c9258bfdc3ee9d45db7d584c1
sosuerasmus <sosuerasmus92@gmail.com>
2026-01-31 13:46:43 +0000

    This is my first website designed for Rockson Enterprise

---
+++ shop-data.js
@@ -0,0 +1,49 @@
+// shop-data.js
+const allProducts = [
+    {
+        name: "iPhone 15 Pro",
+        price: "GH₵ 11,500",
+        category: "phones",
+       img: src="images/Apple 15pro.jpg"
+    },
+    {
+        name: "Samsung Galaxy S23",
+        price: "GH₵ 9,800",
+        category: "phones",
+        img: "images/galaxyS23.jpg"
+    },
+    {
+        name: "Teckno Phantom X2",
+        price: "GH₵ 4,500",
+        category: "phones",
+        img: "images/tecknoPhantomX2.jpg"
+    },
+    {
+        name: "Itel A56 pro",
+        price: "GH₵ 1,450",
+        category: "phones",
+        img: "images/anddroid phones.jpg"
+    },
+    
+    {
+        name: "Oraimo Fast Charger",
+        price: "GH₵ 200",
+        category: "accessories",
+        img: "images/charger.jpg"
+    },
+    {
+         name: "Case",
+         price: "GH₵ 45",
+        category: "accessories",
+        img: "images/case.jpg"
+    },
+
+    {
+    
+    name: "Series 9 Watch",
+    price: "GH₵ 1,200",
+    category: "watches", // MUST BE "watches" to match data-category="watches"
+    img: "images/smart watch.jpg"
+}
+    
+];
